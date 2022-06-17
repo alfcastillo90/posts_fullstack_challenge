f4adf4c053832e1f491c0d5e5db3229e83c2d9d8
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-16 22:14:09 -0400

    ADD: initial commit for post components and routes

--- posts-app/src/app/app-routing.module.ts
+++ posts-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListComponent } from "./components/posts-list/posts-list.component";
+import { AddPostComponent } from "./components/add-post/add-post.component";
+import { PostDetailComponent } from "./components/post-detail/post-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-post' },
+  { path: 'posts-list', component: PostsListComponent },
+  { path: 'add-post', component: AddPostComponent },
+  { path: 'edit-post/:id', component: PostDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

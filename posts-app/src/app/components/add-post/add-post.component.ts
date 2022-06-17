7461426ac259ca2accbd4c803c0aed5eb4620871
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-16 23:10:52 -0400

    ADD: add-post.component.ts

--- posts-app/src/app/components/add-post/add-post.component.ts
+++ posts-app/src/app/components/add-post/add-post.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { PostService } from "../../services/post.service";
 @Component({
-  selector: 'app-add-post',
-  templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.scss']
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.scss']
 })
 export class AddPostComponent implements OnInit {
+  bookForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private postService: PostService
+  ) {
+    this.bookForm = this.formBuilder.group({
+      name: [''],
+      price: [''],
+      description: ['']
+    })
   }
-
+  ngOnInit() { }
+  onSubmit(): any {
+    this.postService.addPost(this.bookForm.value)
+      .subscribe(() => {
+        console.log('Data added successfully!')
+        this.ngZone.run(() => this.router.navigateByUrl('/posts-list'))
+      });
+  }
 }

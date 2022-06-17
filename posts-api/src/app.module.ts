7811b3fa542dbccf20a75fde3cf07c7da52d96e5
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-16 20:11:23 -0400

    DEL: unused files. AppController and AppService

--- posts-api/src/app.module.ts
+++ posts-api/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 
 @Module({
@@ -20,7 +18,5 @@ import { PostModule } from './post/post.module';
     }),
     PostModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

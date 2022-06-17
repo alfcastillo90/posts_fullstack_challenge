53a1c7e32a89235e4d4a948457e7dbde8adf4ba4
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-16 23:54:52 -0400

    add: post detail components with methods for update post information

--- posts-app/src/app/components/post-detail/post-detail.component.ts
+++ posts-app/src/app/components/post-detail/post-detail.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { PostService } from "../../services/post.service";
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent implements OnInit {
+  getId: any;
+  updateForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private activatedRoute: ActivatedRoute,
+    private postService: PostService
+  ) {
+    this.getId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.postService.getPost(this.getId).subscribe(res => {
+      this.updateForm.setValue({
+        userId: res['userId'],
+        title: res['title'],
+        body: res['body']
+      });
+    });
+    this.updateForm = this.formBuilder.group({
+      userId: [''],
+      title: [''],
+      body: ['']
+    })
   }
-
+  ngOnInit() { }
+  onUpdate(): any {
+    this.postService.updatePost(this.getId, this.updateForm.value)
+      .subscribe(() => {
+        console.log('Data updated successfully!')
+        this.ngZone.run(() => this.router.navigateByUrl('/posts-list'))
+      })
+  };
 }

9d39129d526cf6008c7bb9bcaf28f4f2f220fe34
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-06-18 01:09:08 -0400

    UPD: new styles with angular material in posts list component

--- posts-app/src/app/components/posts-list/posts-list.component.ts
+++ posts-app/src/app/components/posts-list/posts-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Post } from "../../model/post";
 import { PostService } from "../../services/post.service";
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-posts-list',
@@ -8,24 +10,41 @@ import { PostService } from "../../services/post.service";
   styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent implements OnInit {
-
+  dataSource: MatTableDataSource<Post>;
+  displayedColumns: string[] = [
+    'postId',
+    'userId',
+    'title',
+    'body',
+    'action',
+  ];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   posts:Post[] = [];
 
-  constructor(private postService: PostService) { }
-
-  ngOnInit(): void {
-    this.postService.getPosts().subscribe(res => {
-      console.log(res)
-      this.posts = res as Post[];
+  constructor(private postService: PostService) {
+    this.postService.getPosts().subscribe((data: Post[]) => {
+      this.posts = data;
+      this.dataSource = new MatTableDataSource<Post>(this.posts);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
     });
   }
 
-  delete(id:any, i:any) {
-    console.log(id);
+
+  ngOnInit(): void {}
+
+  delete(index: number, post: Post) {
     if(window.confirm('Do you want to go ahead?')) {
-      this.postService.deletePost(id).subscribe((res) => {
-        this.posts.splice(i, 1);
-      })
+      const data = this.dataSource.data;
+
+      data.splice(
+        this.paginator.pageIndex * this.paginator.pageSize + index,
+        1
+      );
+
+      this.dataSource.data = data;
+      this.postService.deletePost(post.postId).subscribe();
     }
   }
 }
